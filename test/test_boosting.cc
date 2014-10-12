51a66ef3beed27b0c99d9c118ff778d9e8a1f9f0
Yung Siang Liau <liauys@gmail.com>
2014-10-12 20:29:48 +0800

    Add SUSY to booster tests

--- test/test_boosting.cc
+++ test/test_boosting.cc
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "data.h"
 #include "boosting.h"
+#include "eval.h"
 
 #include <cstdio>
 #include <cmath>
@@ -18,19 +19,31 @@ TEST(BoosterTest, Iris) {
   Booster booster(50, 0.1, 8, 100, 100);
   booster.Train(data);
 
-  LOG(INFO) << "Predicting...";
-
   std::vector<double> preds = booster.Predict(data);
-  std::vector<double> targets = data.GetTargets();
-  double hit = 0.0;
-  for(unsigned int i = 0; i < preds.size(); ++i) {
-    if (std::fabs(preds[i] - targets[i]) == 0) {
-      hit += 1;
-    }
-    if (i > 120 || true) {
-      printf("%f vs %f\n", targets[i], preds[i]);
-    }
-  }
-  double acc = hit / preds.size();
-  LOG(INFO) << "ACC: " << acc;
+  Accuracy metric;
+  double score = metric.Evaluate(preds, data);
+
+  printf("Iris training accuracy: %.6f\n", score);
+
+  ASSERT_NEAR(1.0, score, 1e-6);
+};
+
+TEST(BoosterTest, SUSY) {
+  DataMatrix data_train;
+  data_train.Load("bin/data/susy.svmlight.train.1k");
+  ASSERT_EQ(1000, (int)data_train.Size());
+
+  Booster booster(20, 0.05, 7, 80, 80, 1);
+  booster.Train(data_train);
+
+  DataMatrix data_eval;
+  data_eval.Load("bin/data/susy.svmlight.eval.1k");
+  ASSERT_EQ(1000, (int)data_eval.Size());
+  std::vector<double> predictions = booster.Predict(data_eval);
+  Accuracy metric;
+  double score = metric.Evaluate(predictions, data_eval);
+
+  printf("SUSY validation accuracy: %.6f\n", score);
+
+  ASSERT_NEAR(0.754, score, 1e-6);
 };
