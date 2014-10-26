904e44dc738cce2f9537c610e9b85ead275e28a7
Yung Siang Liau <liauys@gmail.com>
2014-10-26 21:27:04 +0800

    Decide which part of data to load using world info

--- examples/susy/susy.cc
+++ examples/susy/susy.cc
@@ -15,20 +15,25 @@ int main(int argc, char** argv) {
   mpi::communicator& world = MPIHandle::Get().world;
 
   char filename_data_train[1000];
-  sprintf(filename_data_train,
-      "../../Scripts/susy/susy.svmlight.train.50k.p%d", world.rank()+1);
-
   //char filename_data_eval[1000];
+  sprintf(filename_data_train, "../../Scripts/susy/susy.svmlight.train.4500k");
+
+  int num_k_total_samples = 1000;
+  int num_per_node = (int)((float)num_k_total_samples*1000 / world.size());
+  int num_skips = world.rank() * num_per_node;
+
+  LOG(INFO) << "Number of samples per node: " << num_per_node;
+  LOG(INFO) << "Skips for this node: " << num_skips;
 
   DataMatrix data_train;
-  data_train.Load(filename_data_train);
+  data_train.Load(filename_data_train, num_skips, num_per_node);
   LOG(INFO) << "Training data size: " << data_train.Size()
             << " x "
             << data_train.Dimension();
 
   DataMatrix data_eval;
   data_eval.Load("../../Scripts/susy/susy.svmlight.eval.50k");
-  LOG(INFO) << "Training data size: " << data_eval.Size()
+  LOG(INFO) << "Validation data size: " << data_eval.Size()
             << " x "
             << data_eval.Dimension();
 
